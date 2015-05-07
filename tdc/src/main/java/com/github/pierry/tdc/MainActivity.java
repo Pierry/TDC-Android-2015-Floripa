28e1030f09e41c2db3342451931765cc307b15c6
Pierry Borges <pieerry@gmail.com>
2015-05-06 22:50:39 -0300

    Adicionado anotações básicas na MainActivity

--- tdc/src/main/java/com/github/pierry/tdc/MainActivity.java
+++ tdc/src/main/java/com/github/pierry/tdc/MainActivity.java
@@ -1,37 +1,70 @@
 package com.github.pierry.tdc;
 
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
 
+// Criando o OptionMenu e iniciando a Activity
+@OptionsMenu(R.menu.menu_main) @EActivity(R.layout.activity_main)
 public class MainActivity extends ActionBarActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  // Instanciando as views
+  @ViewById TextView tvTitulo;
+  @ViewById TextView tvProgresso;
+  @ViewById Button btnTitulo;
+  @ViewById Button btnProgresso;
+
+  private boolean alterado = true;
+  private static final String TITULO_PADRAO = "Titulo Padrão";
+  private static final String TITULO_ALTERADO = "Titulo Alterado";
+
+  // Executa o metódo após instanciar as views
+  @AfterViews void alterarActionBar() {
+    getSupportActionBar().setTitle("Android Annotations");
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
+  @Click void btnTitulo() {
+    alterarTitulo();
   }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
+  @Click void btnProgresso() {
+    alterarProgresso();
+  }
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+  // Atualiza a view tvTitulo (Necessário para atualizar views)
+  @UiThread void alterarTitulo() {
+    if (alterado) {
+      tvTitulo.setText(TITULO_PADRAO);
+    } else {
+      tvTitulo.setText(TITULO_ALTERADO);
     }
+    alterado = !alterado;
+  }
 
-    return super.onOptionsItemSelected(item);
+  // Executa tarefas em background
+  @Background void alterarProgresso() {
+    for (int i = 0; i <= 100000; i++) {
+      // Atualiza a view com o percentual carregado
+      atualizarTvProgresso(i/1000);
+    }
+  }
+
+  // Atualiza o contador
+  @UiThread void atualizarTvProgresso(int valor){
+    tvProgresso.setText(String.valueOf(valor) + "%");
+  }
+
+  // Click do menu settings
+  @OptionsItem(R.id.action_settings) void btnActionSettings() {
+    Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
   }
 }
